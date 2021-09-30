0c8f6c648328155896092c0fa306e4a5a37423f4
perchatek <merybaltar@gmail.com>
2021-09-30 20:23:34 -0300

    initial commit

---
+++ frontend-challenge-bb/src/components/ActivityCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="activityCardLarge">
+    <div>
+      <img :src="activity.image" :alt="activity.name" class="acImg" />
+    </div>
+    <div class="acHeader">
+      <h2>{{ activity.name }}</h2>
+      <img v-if="activity.participants === 1" :src="people1SVG" class="acPeopleSVG" />
+      <img v-else-if="activity.participants === 2" :src="people2SVG" class="acPeopleSVG" />
+      <img v-if="activity.participants > 2" :src="people3SVG" class="acPeopleSVG" />
+    </div>
+    <p class="acLocation">
+      <img :src="locationSVG" class="acLocationSVG" />
+      {{ activity.location }}
+    </p>
+    <p class="acDescription">{{ activity.description }}</p>
+    <h3 class="acPoints">{{ activity.market_price }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["activity"],
+  data() {
+    return {
+      locationSVG: require("../assets/LocationPink.svg"),
+      people1SVG: require("../assets/Personas1.svg"),
+      people2SVG: require("../assets/PersonasPara2.svg"),
+      people3SVG: require("../assets/Personas2+.svg"),
+    };
+  },
+};
+</script>
+<!-- https://codeburst.io/managing-svg-images-in-vue-js-applications-88a0570d8e88*/ -->
---
+++ frontend-challenge-bb/src/components/AllActivitiesView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <activity-card
+      v-for="activity in activities"
+      :key="activity.id"
+      :activity="activity"
+    />
+  </div>
+</template>
+
+<script>
+import ActivityCard from "./ActivityCard.vue";
+export default {
+  components: { ActivityCard },
+  inject: ["activities"],
+};
+</script>
